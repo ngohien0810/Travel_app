d0606126ee0c913d839fe7fff7748384503277da
nhuydev1704 <86212912+nguyennhuy1704@users.noreply.github.com>
2023-01-03 20:50:58 +0700

    cap nhat bottom tab

--- src/app/navigation/bottom-tab.tsx
+++ src/app/navigation/bottom-tab.tsx
@@ -1,76 +1,123 @@
-import { Image, StyleSheet, Text, View } from 'react-native';
-import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import AnimatedTabBar, { TabsConfigsType } from 'curved-bottom-navigation-bar';
-import { APP_SCREEN } from './screen-types';
-import { Home } from '@features/authentication/home';
-import { createStackNavigator } from '@react-navigation/stack';
-import TourScreen from '@features/authentication/tour';
-import { WIDTH_SCREEN } from '@theme';
-import { icons } from '@assets/icon';
 import { images } from '@assets/image';
+import { Home } from '@features/authentication/home';
+import TourScreen from '@features/authentication/tour';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { APP_SCREEN } from './screen-types';
+
+import * as Animatable from 'react-native-animatable';
 import FavoriteScreen from '@features/authentication/favorite';
 import UserScreen from '@features/authentication/user';
+
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
-const tabs: TabsConfigsType = {
-    [APP_SCREEN.HOME]: {
-        icon: ({ progress, focused }) => (
-            <Image style={{ tintColor: focused ? '#21A8B0' : '#C2C2C2', height: 30, width: 30 }} source={images.home} />
-        ),
-        renderTitle: () => (
-            <View>
-                <Text>Trang chủ</Text>
-            </View>
-        ),
-    },
-    [APP_SCREEN.TOUR]: {
-        icon: ({ progress, focused }) => (
-            <Image style={{ tintColor: focused ? '#21A8B0' : '#C2C2C2', height: 30, width: 30 }} source={images.tour} />
-        ),
-        renderTitle: () => (
-            <View>
-                <Text>Lộ trình</Text>
-            </View>
-        ),
-    },
-    [APP_SCREEN.FAVORITE]: {
-        icon: ({ progress, focused }) => (
-            <Image
-                style={{ tintColor: focused ? '#21A8B0' : '#C2C2C2', height: 30, width: 30 }}
-                source={images.favorite}
-            />
-        ),
-        renderTitle: () => (
-            <View>
-                <Text>Yêu thích</Text>
-            </View>
-        ),
-    },
-    [APP_SCREEN.USER]: {
-        icon: ({ progress, focused }) => (
-            <Image style={{ tintColor: focused ? '#21A8B0' : '#C2C2C2', height: 30, width: 30 }} source={images.user} />
-        ),
-        renderTitle: () => (
-            <View>
-                <Text>Tài khoản</Text>
-            </View>
-        ),
-    },
+const animate1 = {
+    0: { scale: 0.5, translateY: 7 },
+    0.92: { translateY: -34 },
+    1: { scale: 1.2, translateY: -24 },
 };
+const animate2 = { 0: { scale: 1.2, translateY: -24 }, 1: { scale: 1, translateY: 7 } };
+
+const circle1 = {
+    0: { scale: 0 },
+    0.3: { scale: 0.3 },
+    0.5: { scale: 0.5 },
+    0.8: { scale: 0.7 },
+    1: { scale: 1 },
+};
+const circle2 = { 0: { scale: 1 }, 1: { scale: 0 } };
+
+const TabButton = (props: any) => {
+    const { item, onPress, accessibilityState } = props;
+    const focused = accessibilityState.selected;
+    const viewRef: any = React.useRef(null);
+    const circleRef: any = React.useRef(null);
+    const textRef: any = React.useRef(null);
+
+    React.useEffect(() => {
+        if (focused) {
+            viewRef.current.animate(animate1);
+            circleRef.current.animate(circle1);
+            textRef.current.transitionTo({ scale: 1 });
+        } else {
+            viewRef.current.animate(animate2);
+            circleRef.current.animate(circle2);
+            textRef.current.transitionTo({ scale: 0 });
+        }
+    }, [focused]);
+
+    return (
+        <TouchableOpacity onPress={onPress} activeOpacity={1} style={styles.container}>
+            <Animatable.View ref={viewRef} duration={500} style={styles.container}>
+                <View style={styles.btn}>
+                    <Animatable.View ref={circleRef} style={styles.circle} />
+                    {/* <IconSvg
+                        type={item.type}
+                        name={item.inActiveIcon}
+                        color={focused ? Colors.white : Colors.primary}
+                    /> */}
+                    <Image
+                        style={{ tintColor: focused ? '#fff' : '#C2C2C2', height: 25, width: 25 }}
+                        source={item?.icon}
+                    />
+                </View>
+                <Animatable.Text ref={textRef} style={styles.text}>
+                    {item.label}
+                </Animatable.Text>
+            </Animatable.View>
+        </TouchableOpacity>
+    );
+};
+const BottomTabArr = [
+    {
+        route: APP_SCREEN.HOME,
+        label: 'Trang chủ',
+        icon: images.home,
+        component: Home,
+    },
+    {
+        route: APP_SCREEN.TOUR,
+        label: 'Lịch trình',
+        icon: images.tour,
+        component: TourScreen,
+    },
+    {
+        route: APP_SCREEN.FAVORITE,
+        label: 'Yêu thích',
+        icon: images.favorite,
+        component: FavoriteScreen,
+    },
+    {
+        route: APP_SCREEN.USER,
+        label: 'Thông tin',
+        icon: images.user,
+        component: UserScreen,
+    },
+];
 
 const MainBottomTab = () => (
     <Tab.Navigator
         screenOptions={{
             headerShown: false,
+            tabBarStyle: styles.tabBar,
         }}
-        tabBar={(props) => <AnimatedTabBar tabs={tabs} {...props} titleShown={true} barWidth={WIDTH_SCREEN} />}
     >
-        <Tab.Screen name={APP_SCREEN.HOME} component={Home} />
-        <Tab.Screen name={APP_SCREEN.TOUR} component={TourScreen} />
-        <Tab.Screen name={APP_SCREEN.FAVORITE} component={FavoriteScreen} />
-        <Tab.Screen name={APP_SCREEN.USER} component={UserScreen} />
+        {BottomTabArr.map((item, index) => {
+            return (
+                <Tab.Screen
+                    key={index}
+                    name={item.route}
+                    component={item.component}
+                    options={{
+                        tabBarShowLabel: false,
+                        tabBarButton: (props) => <TabButton {...props} item={item} />,
+                    }}
+                />
+            );
+        })}
     </Tab.Navigator>
 );
 
@@ -89,4 +136,40 @@ const BottomTab = () => {
 
 export default BottomTab;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    tabBar: {
+        height: 70,
+        position: 'absolute',
+        bottom: 16,
+        right: 16,
+        left: 16,
+        borderRadius: 16,
+    },
+    btn: {
+        width: 44,
+        height: 44,
+        borderRadius: 25,
+        borderWidth: 4,
+        borderColor: '#fff',
+        backgroundColor: '#fff',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    circle: {
+        ...StyleSheet.absoluteFillObject,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#21A8B0',
+        borderRadius: 25,
+    },
+    text: {
+        fontSize: 10,
+        textAlign: 'center',
+        color: '#21A8B0',
+    },
+});
